43bb78218e927b3ecc56e888dce82aa135c4362c
Thomas Brandauer <thomas@bitcraft.at>
2023-05-10 09:32:42 +0200

    feat: upload dist

---
+++ dist/config.d.ts
@@ -0,0 +1,23 @@
+import { a as NitroConfig } from './nitro-d1eed093.js';
+import 'unenv';
+import 'unimport';
+import 'unimport/unplugin';
+import 'rollup-plugin-visualizer';
+import 'hookable';
+import 'consola';
+import 'chokidar';
+import '@rollup/plugin-commonjs';
+import '@rollup/plugin-wasm';
+import 'unstorage';
+import 'http-proxy';
+import 'h3';
+import 'c12';
+import '@vercel/nft';
+import 'rollup';
+import 'ofetch';
+import 'esbuild';
+import '@rollup/pluginutils';
+
+declare function defineNitroConfig(config: NitroConfig): NitroConfig;
+
+export { defineNitroConfig };
